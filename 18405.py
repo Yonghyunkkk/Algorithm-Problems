2cf4c45bc34802500203ceca545999b8b2ab63a0
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2022-05-31 17:25:13 +0900

    BaekJoon 18405

--- 18405.py
+++ 18405.py
@@ -60,3 +60,41 @@ for i in range(s):
                 records[j][k] = queue
 
 print(graph[x-1][y-1])
+
+
+n,k = map(int,input().split())
+
+graph = []
+data = []
+
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+    
+    for j in range(n):
+        if graph[i][j] != 0:
+            data.append((graph[i][j],0,i,j))
+
+s,target_x,target_y = map(int,input().split())
+
+data.sort()
+queue = deque(data)
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+while queue:
+    virus, time, x, y = queue.popleft()
+
+    if time == s:
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
+                queue.append((virus,time+1,nx,ny))
+
+print(graph[target_x-1][target_y-1])
